2f15a28f45f9458d72ff5978d78f4453603b99ce
happy-44 <happymittal129@gmail.com>
2022-07-08 10:17:46 +0530

    all files

--- pokemon(assignment-1)/script.js
+++
@@ -1,5 +0,0 @@
-let bt = document.querySelector("button");
-bt.addEventListener("click",function(){
-    alert("Are you sure you want to proceed");
-    window.location="index2.html";
-})
--- pokemon(assignment-1)/script2.js
+++
@@ -1,5 +0,0 @@
-let btn=document.querySelector('button');
-btn.addEventListener('click',fn);
-function fn(){
-    window.location="index.html";
-}
